8da5136e09828d6be53af62fd9bdaf19501837e2
Matthew Jiao <jiaomatthew222@gmail.com>
2021-09-09 18:18:41 -0400

    styling image container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "max-w-sm rounded overflow-hidden shadow-lg">
+      <img src = "https://source.unsplash.com/random" alt = "" className = "w-full"/>
+      <div className = "px-6 py-4">
+        <div className = "font-bold text-purple-500 text-xl mb-2">
+          Photo by Sam
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong> 10000
+          </li>
+          <li>
+            <strong>Downloads:</strong> 76
+          </li>
+          <li>
+            <strong>Likes:</strong> 33
+          </li>
+        </ul>
+      </div>
+
+      <div className = "px-6 py-4">
+        <div className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tags
+        </div>
+      </div>
     </div>
   );
 }
