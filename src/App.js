dee34dfe0b2862a22bfcb730c0549429beb85e76
Matthew Jiao <jiaomatthew222@gmail.com>
2021-09-10 12:11:22 -0400

    infrastructure ready

--- src/App.js
+++ src/App.js
@@ -21,10 +21,11 @@ function App() {
   return (
     <div className = "container mx-auto">
       <ImageSearch searchText = {(text) => setTerm(text)}/>
+      {!isLoading && images.length === 0 ? <h1 className = "text-5xl text-center mx-auto mt-32">No Images Found</h1>: '' }
       { isLoading ? <h1 className = "text-6xl text-center mx-auto mt-32">Loading</h1> :
       <div className = "grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key = {image.id} image = {image}/>
+        {images.map((image, index) => (
+          <ImageCard key = {index} image = {image}/>
         ))}
       </div>
       }
