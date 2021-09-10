61c24c89dd3178a52e6efeca2fcfe9040122d3c5
Matthew Jiao <jiaomatthew222@gmail.com>
2021-09-10 01:45:35 -0400

    isLoading state with image mapping

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
-
+import React, {useState, useEffect} from 'react'
+import ImageCard from './components/ImageCard'
 
 function App() {
-  return (
-    <div className = "max-w-sm rounded overflow-hidden shadow-lg">
-      <img src = "https://source.unsplash.com/random" alt = "" className = "w-full"/>
-      <div className = "px-6 py-4">
-        <div className = "font-bold text-purple-500 text-xl mb-2">
-          Photo by Sam
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong> 10000
-          </li>
-          <li>
-            <strong>Downloads:</strong> 76
-          </li>
-          <li>
-            <strong>Likes:</strong> 33
-          </li>
-        </ul>
-      </div>
+  const [images, setImages] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [term, setTerm] = useState('')
 
-      <div className = "px-6 py-4">
-        <span className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tags
-        </span>
-        <span className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tags
-        </span>
-        <span className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tags
-        </span>
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}q=${term}&ima
+    ge_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits)
+      setLoading(false)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+
+  return (
+    <div className = "container mx-auto">
+      <div className = "grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key = {image.id} image = {image}/>
+        ))}
       </div>
     </div>
   );
