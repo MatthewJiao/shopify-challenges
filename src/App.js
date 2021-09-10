72b124fa40cc2417693d52f3a19780616bd986c5
Matthew Jiao <jiaomatthew222@gmail.com>
2021-09-10 12:00:19 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, {useState, useEffect} from 'react'
 import ImageCard from './components/ImageCard'
+import ImageSearch from './components/ImageSearch'
 
 function App() {
   const [images, setImages] = useState([])
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const [term, setTerm] = useState('')
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}q=${term}&ima
-    ge_type=photo&pretty=true`)
-    .then(res => res.json())
-    .then(data => {
-      setImages(data.hits)
-      setLoading(false)
-    })
-    .catch(err => console.log(err))
-  }, [])
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
 
 
   return (
     <div className = "container mx-auto">
+      <ImageSearch searchText = {(text) => setTerm(text)}/>
+      { isLoading ? <h1 className = "text-6xl text-center mx-auto mt-32">Loading</h1> :
       <div className = "grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key = {image.id} image = {image}/>
         ))}
       </div>
+      }
     </div>
   );
 }
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -25,8 +25,8 @@ const ImageCard = ({image}) => {
       </div>
 
       <div className = "px-6 py-4">
-        {tags.map(tag => (
-            <span key = {image.id} className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        {tags.map((tag, index) => (
+            <span key = {index} className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
             #{tag}
           </span>
         ))}
