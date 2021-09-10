1ecef5628c2426988c9f20b3855e7e17f3b3d972
Matthew Jiao <jiaomatthew222@gmail.com>
2021-09-10 19:39:37 -0400

    nasa api integrate

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ function App() {
   const [term, setTerm] = useState('vacation')
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&count=8`)
       .then(res => res.json())
       .then(data => {
-        setImages(data.hits);
+        setImages(data);
         setIsLoading(false);
+        console.log(data)
       })
       .catch(err => console.log(err));
   }, [term]);
@@ -20,10 +21,8 @@ function App() {
 
   return (
     <div className = "container mx-auto">
-      <ImageSearch searchText = {(text) => setTerm(text)}/>
-      {!isLoading && images.length === 0 ? <h1 className = "text-5xl text-center mx-auto mt-32">No Images Found</h1>: '' }
-      { isLoading ? <h1 className = "text-6xl text-center mx-auto mt-32">Loading</h1> :
-      <div className = "grid grid-cols-3 gap-4">
+      { isLoading ? <h1 className = "text-6xl text-center mx-auto mt-32">Loading ...</h1> :
+      <div className = "grid grid-cols-3">
         {images.map((image, index) => (
           <ImageCard key = {index} image = {image}/>
         ))}
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,36 +1,42 @@
 import React from 'react'
+import parse from 'de-noun-parser';
 
 
 const ImageCard = ({image}) => {
-    const tags = image.tags.split(',')
+  const tags = ["test1", "test1", "test3"]
+  
+  
 
     return (
         <div className = "max-w-sm rounded overflow-hidden shadow-lg">
-      <img src = {image.webformatURL} alt = "" className = "w-full"/>
+      <img src = {image.hdurl} alt = "" className = "w-full"/>
       <div className = "px-6 py-4">
         <div className = "font-bold text-purple-500 text-xl mb-2">
-          Photo by {image.user}
+          {image.title}
         </div>
         <ul>
           <li>
-            <strong>Views:</strong> {image.views}
+            <strong>Date:</strong> {image.date}
           </li>
           <li>
-            <strong>Downloads:</strong> {image.downloads}
+            <strong>Description:</strong> {image.date}
           </li>
           <li>
-            <strong>Likes:</strong> {image.likes}
+            <strong>Likes:</strong> {image.date}
           </li>
         </ul>
       </div>
 
       <div className = "px-6 py-4">
+        
         {tags.map((tag, index) => (
             <span key = {index} className = "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
             #{tag}
           </span>
         ))}
+        
       </div>
+      
     </div>
     )
 }
