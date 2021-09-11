93836581f71aced2fb2970e7ce8d89ea1f0f7c10
Matthew Jiao <jiaomatthew222@gmail.com>
2021-09-11 19:32:57 -0400

    loading state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import ImageCard from './components/ImageCard'
 import ImageSearch from './components/ImageSearch'
 import NavbarC from './components/NavbarC'
+import Loading from './components/Loading'
 
 function App() {
   const [images, setImages] = useState([])
@@ -25,7 +26,7 @@ function App() {
       <NavbarC/>
       <ImageSearch searchText = {(text) => setTerm(text)}/>
       {!isLoading && images.length === 0 ? <h1 className = "text-5xl text-center mx-auto mt-32">No Images Found</h1>: '' }
-      { isLoading ? <h1 className = "text-6xl text-center mx-auto mt-32">Loading</h1> :
+      { isLoading ? <Loading/> :
       <div className = "grid grid-cols-3 gap-4">
         {images.map((image, index) => (
           <ImageCard key = {index} image = {image}/>
